e98e3bfcbe769fd3bbc011b5116f902599cc0f5b
Creastv <piotr@roial.pl>
2023-01-17 11:36:55 +0100

    first commit

--- templates-parts/parts/slider.php
+++ templates-parts/parts/slider.php
@@ -7,14 +7,30 @@ $slider    = get_field('slider');
         <div class="swiper slider-home">
             <div class="swiper-wrapper">
                 <?php foreach ($slider as $slide ) { ?>
+                <?php 
+                    $link = $slide['link'];
+                    if( $link ){ 
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                    }
+                ?>
                 <div class="swiper-slide">
                     <div class="slide <?php echo $slide['image'] ? 'c-white' : '' ?> ">
-                        <div class="slider__content">
-                            <h1 class="h1 h1-slider"><?php echo $slide['title']; ?></h1>
-                            <?php if($slide['subtitle']) { ?>
-                            <p><?php echo $slide['subtitle']; ?></p>
+                        <?php if($link ) { ?>
+                        <a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
                             <?php } ?>
-                        </div>
+                            <div class="slider__content">
+
+                                <h1 class="h1 h1-slider"><?php echo $slide['title']; ?></h1>
+                                <?php if($slide['subtitle']) { ?>
+                                <p><?php echo $slide['subtitle']; ?></p>
+                                <?php } ?>
+
+                            </div>
+                            <?php if($slide['link']) { ?>
+                        </a>
+                        <?php } ?>
                         <?php if($slide['image']) { ?>
                         <div class="img" style="background-image: url(<?php echo $slide['image']; ?>)">
                             <div class="bg"></div>
