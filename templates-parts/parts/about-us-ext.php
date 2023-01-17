94beb9c8b2416bacbad1de3ebb3461bfb86c8d73
Creastv <piotr@roial.pl>
2023-01-17 16:54:56 +0100

    fixing

--- templates-parts/parts/about-us-ext.php
+++ templates-parts/parts/about-us-ext.php
@@ -1,11 +1,41 @@
 <?php
 $leftColumn   = get_field('left_column');
-$rightColumn   = get_field('right_column');
+
+$leftc       = get_field('left_column_baner');
+
+$icon         = $leftc['icon'];
+$link = $leftB['button'];
+if( $link ){
+    $link_url = $link['url'];
+    $link_title = $link['title'];
+    $link_target = $link['target'] ? $link['target'] : '_self';
+}
 ?>
 <section id="about-us-ex">
     <div class="about-us__wraper">
         <div class="about-us__col">
-            <?php print $leftColumn; ?>
+            <div class="about-us__baner">
+                <?php if($icon) { ?>
+                <?php  echo wp_get_attachment_image( $icon, 'icon' ); ?>
+                <br><br>
+                <?php } ?>
+                <?php foreach($leftc['info'] as $info) { ?>
+                <div class="about-us__baner__content">
+                    <?php if($info['span']) { ?>
+                    <span><?php echo $info['span']; ?></span>
+                    <?php } ?>
+                    <?php if($info['number']) { ?>
+                    <p><b><b><?php echo $info['number']; ?></b><?php echo $info['big_span']; ?></b></p>
+                    <?php } ?>
+                    <?php if($info['span_sec']) { ?>
+                    <span><?php echo $info['span_sec']; ?></span>
+                    <?php } ?>
+                </div>
+                <?php } ?>
+                <?php if($link){?>
+                <a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                <?php } ?>
+            </div>
         </div>
         <div class="about-us__col">
             <?php echo $rightColumn; ?>
--- templates-parts/parts/about-us.php
+++ templates-parts/parts/about-us.php
@@ -1,11 +1,41 @@
 <?php
 $leftColumn   = get_field('left_col');
 $banner       = get_field('banner_as'); 
+
+$leftB        = get_field('left_col_baner');
+$icon         = $leftB['icon'];
+$link = $leftB['button'];
+if( $link ){
+    $link_url = $link['url'];
+    $link_title = $link['title'];
+    $link_target = $link['target'] ? $link['target'] : '_self';
+}
 ?>
 <section id="about-us">
     <div class="about-us__wraper">
         <div class="about-us__col">
-            <?php print $leftColumn; ?>
+            <div class="about-us__baner">
+                <?php if($icon) { ?>
+                <?php  echo wp_get_attachment_image( $icon, 'icon' ); ?>
+                <br><br>
+                <?php } ?>
+                <?php foreach($leftB['info'] as $info) { ?>
+                <div class="about-us__baner__content">
+                    <?php if($info['span']) { ?>
+                    <span><?php echo $info['span']; ?></span>
+                    <?php } ?>
+                    <?php if($info['number']) { ?>
+                    <p><b><b><?php echo $info['number']; ?></b><?php echo $info['big_span']; ?></b></p>
+                    <?php } ?>
+                    <?php if($info['span_sec']) { ?>
+                    <span><?php echo $info['span_sec']; ?></span>
+                    <?php } ?>
+                </div>
+                <?php } ?>
+                <?php if($link){?>
+                <a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                <?php } ?>
+            </div>
         </div>
         <div class="about-us__col">
             <div class="about-us__cover" style="background-image: url(<?php echo $banner['image']; ?>)">
