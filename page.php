c2044bf99bc106307d66920b4b1c72e5d7c4ae39
Creastv <piotr@roial.pl>
2023-01-18 12:37:35 +0100

    fixing

--- page.php
+++ page.php
@@ -1,11 +1,12 @@
 <?php
 get_header();
 while ( have_posts() ) : the_post(); ?>
+<!-- test -->
 <article id="post-<?php the_ID(); ?>" class="hentry">
     <?php
 	get_template_part( 'templates-parts/header/header', 'title' ); 
 	
-	if(is_page(10)){ //Home
+	if(is_page(array(10, 876))){ //Home
 		get_template_part( 'templates-parts/parts/slider' );
 		get_template_part( 'templates-parts/parts/logos' );
 		get_template_part( 'templates-parts/parts/opinion' );
@@ -14,21 +15,21 @@ while ( have_posts() ) : the_post(); ?>
 		get_template_part('templates-parts/parts/footer', 'form');
 		get_template_part( 'templates-parts/parts/numbers' );
 		get_template_part( 'templates-parts/parts/testimonial' );
-	 } else if (is_page(17)){ // About us
+	 } else if (is_page(array(17, 886))){ // About us
 		get_template_part( 'templates-parts/parts/numbers' );
 		get_template_part( 'templates-parts/parts/about-us-ext' );
 		get_template_part( 'templates-parts/parts/business-areas' );
 		// get_template_part( 'templates-parts/parts/testimonial' );
-	 } else if (is_page(21)){ //Case study
+	 } else if (is_page(array(21, 899))){ //Case study
 		get_template_part( 'templates-parts/parts/case-study' );
 		get_template_part( 'templates-parts/parts/banner' );
 		get_template_part( 'templates-parts/parts/testimonial-2' );
-	 } else if (is_page(19)){ //service
+	 } else if (is_page(array(19, 895))){ //service
 		get_template_part( 'templates-parts/parts/business-areas' );
 		get_template_part( 'templates-parts/parts/testimonial' );
-	  } else if (is_page(array(95, 589))){ //Contact us
+	  } else if (is_page(array(95, 589, 874))){ //Contact us
 		get_template_part( 'templates-parts/parts/contact-us' );
-	  } else if (is_page(110)){ //carrer
+	  } else if (is_page(array(110, 889))){ //carrer
 		get_template_part( 'templates-parts/parts/carrera' );
 	  }
 	 ?>
