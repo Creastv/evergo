e5d23b87c6a9db166f62905dabd288f281be0deb
Creastv <piotr@roial.pl>
2023-01-25 09:27:57 +0100

    fixing

--- templates-parts/header/header-lang-swicher.php
+++ templates-parts/header/header-lang-swicher.php
@@ -1,27 +1,27 @@
-<?php $languages = icl_get_languages('skip_missing=1&order=dsc&orderby=id');
-                        if(count($languages)){
-                            echo '<div class="lang-swicher">';
-                            // echo '<ul>';
-                            foreach($languages as $l){
-                                if(!$l['active']) {
-                                echo '<li>';
-                                echo '<a href="'.$l['url'].'">';
-                                } else {
-                                echo '<li class="active">';
-                                }
-
-                                if($l['native_name'] !== 'English') {
-                                    echo 'PL';
-                                } else {
-                                    echo 'EN';
-                                }
-                                
-                                if(!$l['active']) {
-                                echo '</a>';
-                                }
-                                echo '</li>';
-                            }
-                            // echo '<ul>';
-                            echo '</div>';
-                        }
-                    ?>
+<?php
+$languages = icl_get_languages('skip_missing=1&order=dsc&orderby=id');
+if(1 < count($languages)){
+    if(count($languages)){
+     echo '<div class="lang-swicher">';
+        echo '<ul>';
+        foreach($languages as $l){           
+            echo '<li>';
+            if(!$l['active']) {
+                echo '<a href="'.$l['url'].'">';
+            } else {
+                echo '<li class="active">';
+            }
+            if($l['native_name'] !== 'English') {
+                echo 'PL';
+            } else {       
+                echo 'EN';        
+            }          
+            if(!$l['active']) {
+            echo '</a>';
+            }
+            echo '</li>';          
+         }
+        echo '</ul>';
+    echo '</div>';
+    }
+}
